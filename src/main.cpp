44a0352e22a80b98665b571d887d9c70c2d0b574
Jonas Østergaard <Jonasoe@hotmail.com>
2024-01-29 10:39:48 +0100

    works kinda?
    need to test pins, internet, and json parsing

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,9 @@ const char* apiUrl = "https://example.com/api/data";
 
 
 // Define the pins
-const int impulsePin1 = 0; // Impulse pin
-const int impulsePin2 = 1; // Impulse pin
-const int impulsePin3 = 3; // Impulse pin
+const int impulsePin1 = 13; // Impulse pin
+const int impulsePin2 = 16; // Impulse pin
+const int impulsePin3 = 32; // Impulse pin
 const int impulsePin4 = 33; // Impulse pin
 
 
@@ -42,85 +42,176 @@ void impulseDetected2();
 void impulseDetected3();
 void impulseDetected4();
 
-void sendImpulseDataToQueue(String meterId);
-
 void queueDataHandling(void *pvParameters);
 void sendToApi(void *pvParameters);
 
 void apiCall(dataStruct data);
 
+bool setupSdCard();
+bool setupMutex();
+bool setupInterrupts();
+
 // setup starts here
 void setup() {
+
   Serial.begin(115200);
   Serial.println("Starting...");
 
-  // initialize sd card
-  if(!SD_MMC.begin()){
-    Serial.println("Card Mount Failed");
-    while(1);
-  }
-
-  // check if sd card file exists
-  // if it does not exist create it
-  if(!SD_MMC.exists("data.json"))
-  {
-    File file = SD_MMC.open("/data.json", FILE_WRITE);
-    file.close();
-  }
-
-
-  // create semaphore to lock sd-card access
-  sdCardMutex = xSemaphoreCreateMutex();
-
-  if(sdCardMutex == NULL){
-    Serial.println("Mutex creation failed");
-    while(1);
-  }
-
-
   pinMode(impulsePin1, INPUT); // sets pin to input
-  attachInterrupt(impulsePin1, impulseDetected1, RISING); // sets interrupt when pin goes from low to high
-
   pinMode(impulsePin2, INPUT);
-  attachInterrupt(impulsePin2, impulseDetected2, RISING);
-
   pinMode(impulsePin3, INPUT);
-  attachInterrupt(impulsePin3, impulseDetected3, RISING); 
+  pinMode(impulsePin4, INPUT);
 
-  pinMode(impulsePin4, INPUT); 
-  attachInterrupt(impulsePin4, impulseDetected4, RISING);
+  Serial.println("Pins set to input");
+  // delay(2000);
+
+  // WiFi.begin(ssid, password);
+
+  // while(WiFi.status() != WL_CONNECTED){
+  //   delay(500);
+  //   Serial.println("Connecting to WiFi..");
+  // }
+
+  if(!setupSdCard()){
+    Serial.println("sd-card setup failed...");
+    while(1);
+  }
+ 
+  if(!setupMutex()){
+    Serial.println("Mutex Creation Failed");
+    while(1);
+  }
+
+  // delay(2000);
+
+  Serial.println("Starting dataQueue creation");
+
+  // delay(200);
+
+  dataQueue = xQueueCreate(10, sizeof(String));
+
+  // delay(200);
+
+  Serial.println("dataQueue creation complete");
+
+  // delay(2000);
 
 
-  dataQueue = xQueueCreate(100, sizeof(dataStruct));
+
+  
+  
+  if(!setupInterrupts()){
+    Serial.println("Interrupts setup failed...");
+    while(1);
+  }
+
+  // delay(2000);
+    
+  Serial.println("Starting xTaskCreate");
+  // delay(500);
 
   xTaskCreate(                  // create a new rtos  task
     queueDataHandling,          // the name of what function will run
-    "queueDataHandling",        // the name of the task
-    1024,                       // the stack size of the task
+    "Queue Data Handling",      // the name of the task
+    4096,                      // the stack size of the task
     NULL,                       // the parameter passed to the task
     1,                          // the priority of the task
     NULL                        // the task handle
   );
 
+  // delay(2000);
+    Serial.println("First xTaskCreate Completed");
+  // delay(2000);
+
   xTaskCreate(
     sendToApi,
-    "SendToAPI",
-    1024,
+    "Api Call",
+    4096,
     NULL,
     2,
     NULL
   );
 
-  vTaskStartScheduler(); // starts rtos
+  // delay(200);
+  
+  Serial.println("xTaskCreate complete");
+
+
+
+  // delay(2000);
+
+  // vTaskStartScheduler(); // starts rtos
+  // delay(2000);
+  Serial.println("Setup done");
+}
+
+// setup functions
+
+bool setupMutex(){
+  // create semaphore to lock sd-card access
+  while(sdCardMutex == NULL)
+  {
+    // delay(2000);
+    sdCardMutex = xSemaphoreCreateMutex();
+  }
+  // delay(2000);
+  Serial.println("Mutex created");
+  return true;
+}
+
+
+bool setupSdCard(){
+   // initialize sd card
+
+  while (!SD_MMC.begin())
+  {
+    Serial.println("SD-Card Mounting");
+    // delay(500);
+  }
+  Serial.println("SD-Card Mounted");
+  // delay(2000);
+
+  // check if sd card file exists
+  // if it does not exist create it
+
+  if(!SD_MMC.exists("/data.json"))
+  {
+    // delay(2000);
+    Serial.println("File does not exist, creating file");
+    File file = SD_MMC.open("/data.json", FILE_WRITE);
+    file.close();
+  }
+  // delay(2000);
+
+  Serial.println("File exists / is created");
+  return true;
 
 }
+
+bool setupInterrupts(){
+  attachInterrupt(impulsePin1, impulseDetected1, RISING); // sets interrupt when pin goes from low to high
+  // delay(500);
+
+  attachInterrupt(impulsePin2, impulseDetected2, RISING);
+  // delay(500);
+
+  attachInterrupt(impulsePin3, impulseDetected3, RISING); 
+  // delay(500);
+    
+  attachInterrupt(impulsePin4, impulseDetected4, RISING);
+  Serial.println("Interrupts attached");
+
+  return true;
+}
+
 
 // Interrupt functions
 
 void impulseDetected1() {
 
   Serial.println("Impulse detected on impulsePin1!");
-  sendImpulseDataToQueue(meterId1);
+  delay(20);
+  xQueueSendFromISR(dataQueue, &meterId1, 0);
 
 }
 
@@ -128,7 +219,8 @@ void impulseDetected1() {
 void impulseDetected2() {
 
   Serial.println("Impulse detected on impulsePin2!");
-  sendImpulseDataToQueue(meterId2);
+  delay(20);
+  xQueueSendFromISR(dataQueue, &meterId2, 0);
 
 }
 
@@ -136,7 +228,8 @@ void impulseDetected2() {
 void impulseDetected3() {
 
   Serial.println("Impulse detected on impulsePin3!");
-  sendImpulseDataToQueue(meterId3);
+  delay(20);
+  xQueueSendFromISR(dataQueue, &meterId3, 0);
 
 }
 
@@ -144,78 +237,77 @@ void impulseDetected3() {
 void impulseDetected4() {
 
   Serial.println("Impulse detected on impulsePin4!");
-  sendImpulseDataToQueue(meterId4);
+  delay(20);
+  xQueueSendFromISR(dataQueue, &meterId4, 0);
 
-}
-
-
-// function for interrupts to use
-
-void sendImpulseDataToQueue(String meterId){
-
-    xQueueSendFromISR(dataQueue, &meterId, 0);
 }
 
 
 // RTOS Functions
 // need to change if the sd_card library isn't working
 void queueDataHandling(void *pvParameters){
-  
+  vTaskDelay(10000 / portTICK_PERIOD_MS);
+  while(1)
+  {
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println("QueueDataHandling Started");
+
     dataStruct data;
     String meterId;
-    while(1){
 
-      if(xQueueReceive(dataQueue, &meterId, 0))
-      {
-        data.accumulatedValue = ++accumulation;
-        data.meterId = meterId;
-        // take mutex
-        if(xSemaphoreTake(sdCardMutex, portMAX_DELAY) != pdTRUE){
-          Serial.println("Mutex failed to be taken within max delay");
-          continue;
-        }
-
-        // open sd card
-        File file = SD_MMC.open("/data.json", FILE_APPEND);
-
-        // check if file opened
-        if(!file){
-
-          Serial.println("Failed to open file for appending");
-          return;
-
-        }
-
-        // format data as json file
-        String dataString = "{\n\t\"meterId\": \"" + data.meterId + "\",\n\t\"accumulatedValue\": " + String(data.accumulatedValue) + "\n}\n";
-
-        // write data to sd card
-        file.print(dataString);
-
-        // close sd card
-        file.close();
-
-        // give mutex
-        xSemaphoreGive(sdCardMutex);
-
-        // data is remove from queue on recieve
+    if(xQueueReceive(dataQueue, &meterId, 0))
+    {
+      data.accumulatedValue = ++accumulation;
+      data.meterId = meterId;
+      // take mutex
+      if(xSemaphoreTake(sdCardMutex, portMAX_DELAY) != pdTRUE){
+        Serial.println("Mutex failed to be taken within max delay");
+        return;
       }
 
-      // add vtaskdelay() ???
+      // open sd card
+      File file = SD_MMC.open("/data.json", FILE_APPEND);
 
+      // check if file opened
+      if(!file){
 
+        Serial.println("Failed to open file for appending");
+        return;
+
+      }
+
+      // format data as json file
+      String dataString = "{\n\t\"meterId\": \"" + data.meterId + "\",\n\t\"accumulatedValue\": " + String(data.accumulatedValue) + "\n}\n";
+
+      // write data to sd card
+      file.print(dataString);
+
+      // close sd card
+      file.close();
+
+      // give mutex
+      xSemaphoreGive(sdCardMutex);
+
+      // data is remove from queue on recieve
     }
+
+    // add vtaskdelay() ???
+  }
 
 }
 
 
 void sendToApi(void *pvParameters){
-  while(1){
+  vTaskDelay(10000 / portTICK_PERIOD_MS);
+  while(1)
+  {
+    Serial.println("sendToApi Started");
+
     // take mutex
                       // mutex     // max delay
     if(xSemaphoreTake(sdCardMutex, portMAX_DELAY) != pdTRUE){
       Serial.println("Mutex failed to be taken within max delay");
-      continue;
+      return;
     }
 
     // read file from sd card
@@ -250,22 +342,26 @@ void sendToApi(void *pvParameters){
 
     if(error){
       Serial.println("Failed to parse json file");
-      return;
     }
 
-    // json to data struct
-    JsonArray array = doc.as<JsonArray>();
+    if(!error){
 
-    for (int i = 0; i < array.size(); i++)
-    {
-      dataStruct data;
-      data.meterId = array[i]["meterId"].as<String>();
-      data.accumulatedValue = array[i]["accumulatedValue"].as<int>();
-      apiCall(data);
+      // json to data struct
+      JsonArray array = doc.as<JsonArray>();
+
+      
+
+      for (int i = 0; i < array.size(); i++)
+      {
+        dataStruct data;
+        data.meterId = array[i]["meterId"].as<String>();
+        data.accumulatedValue = array[i]["accumulatedValue"].as<int>();
+        apiCall(data);
+      }
     }
-
-    // wait 10 seconds
+    Serial.println("sendToApi Finished");
     vTaskDelay(10000 / portTICK_PERIOD_MS);
+
   }
 }
 
@@ -273,12 +369,7 @@ void sendToApi(void *pvParameters){
 void apiCall(dataStruct data){
 
   // open wifi connection
-  WiFi.begin(ssid, password);
 
-  while(WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
 
   Serial.println("Connected to WiFi");
 
@@ -305,7 +396,7 @@ void apiCall(dataStruct data){
   http.end();
 
   // close wifi connection
-  WiFi.disconnect(true);
+  // WiFi.disconnect(true);
 
 }
 
